c18acf2e7ec329785c747ca0ca0042f8d1db0b72
himanshu6388 <himanshu219181@gmail.com>
2025-02-21 08:08:16 +0530

    updated

---
+++ JsDay04/script.js
@@ -0,0 +1,27 @@
+// Functions
+
+// What is function
+//block of code  which we cann reuse
+function abcd(){
+    // statements
+}
+
+function greet(){
+    console.log("Hello!")
+}
+// function cahlao
+greet();
+// why function
+// jab bhi code turant  nhi chalana  aur aap ko reuse karna h tab function banate h
+function abc(){
+    console.log("Hello World")
+}
+// why we need function
+// To reuuse some code which is too long to write
+
+
+// how  to create function
+// how to call a function
+// how to pass  a parameter to a function
+// how to pass argument to a function
+// types of  functions
