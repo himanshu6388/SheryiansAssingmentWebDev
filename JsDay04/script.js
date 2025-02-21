0c2476e220c18bc3c86da5337d045704a2201cd5
himanshu6388 <himanshu219181@gmail.com>
2025-02-21 08:33:59 +0530

    updated

--- JsDay04/script.js
+++ JsDay04/script.js
@@ -21,7 +21,82 @@ function abc(){
 
 
 // how  to create function
+
+function name(){
+    // statements
+}
+const fnc = ()=>{
+    // statements
+}
 // how to call a function
+
+function hi(){
+    console.log("Hi")
+}
+// calling
+hi();
+
+// What is Parameter -> 
+
 // how to pass  a parameter to a function
+  function  greeting(g){
+
+    console.log(g)
+  }
+
+  greeting("Hello");
+//   greeting("Hi");
+//   greeting("How are You?")
+
+
 // how to pass argument to a function
+function num(a, b,c,d){
+
+    console.log(a, b, c, d)
+  
+}
+num(1,2,3,4,5,6)  //argument
+
+
 // types of  functions
+
+//1. functions statement
+function ab(){
+    // function statement
+}
+
+//2. function expression
+var abcde = function(){
+    // function expression
+}
+
+//3. Anonymous function
+let arr = [1,2,3,4];
+arr.forEach(function(){
+
+    // anonymous function
+})
+
+//4. fat Arrow function
+    let fn = ()=>{
+
+
+    }
+//5. fat arrow with one parameter
+ let func = a =>{
+    // fat arrow with one parameter
+ }
+func(12);
+//6. fat arrow with implicit return
+
+function funnc(){
+     return 10;
+}
+var ans = funnc()
+console.log(ans)
+
+
+ var x = ()=> 12
+ 
+ var  a = x();
+ console.log(a)
