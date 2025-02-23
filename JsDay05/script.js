44bc033336d4ab08440511129ae913de3e6ce802
himanshu6388 <himanshu219181@gmail.com>
2025-02-23 08:13:24 +0530

    updated

--- JsDay05/script.js
+++ JsDay05/script.js
@@ -42,17 +42,67 @@ xy(function(){
 // cb fnc
 
 
+
+
 // first calss fnc -> first class function ek darja hai jo ki js mein fncs ko mila hai , is darje mein kaha jaata hai ki fncs ko aap value ki tarah use kar skate ho
  var a = function(){
     console.log("Hello")
  }
 
  function ab(val){
-    
+
  }
  ab(function(){
 
 
  })
 
-// impure function
+
+
+// impure function -> A function that modifies external state
+total = 0 ; 
+function addToTotal(){
+    total += a; // modifies external state
+}
+addToTotal(5);
+console.log(total); //5
+
+
+
+// pure function -> a function without side efffects (does not modiify external state)
+function  abcde(a, b){
+    return a + b; //no external modfication
+}
+console.log(add(2,3));
+
+
+
+
+
+// global scope
+var a =12;
+function aaa(){
+    console.log(a)
+}
+
+
+
+
+// local scope 
+function bbb(){
+    var a = 12;
+    console.log(a)
+}
+
+
+
+
+// closures ->  ek function hai joismein function return karta hai ek aur function and returned fnction mein aap use karte ho parent fucntion koi data
+function adb(){
+    var a = 12;
+    return function(){
+        console.log(a)
+    }
+}
+
+var ans = abb();
