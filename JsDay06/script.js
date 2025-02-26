d2e0ea4c0669f6b03a3a6a6e7a4cb93648e238f0
himanshu6388 <himanshu219181@gmail.com>
2025-02-26 08:04:24 +0530

    updated

---
+++ JsDay06/script.js
@@ -0,0 +1,46 @@
+// arrays and objects
+
+// var a = 12;
+
+ var b = 33;
+
+ var c = [12, 13]
+//  arry ek se jayda vlues ko ek sath store karnre wala data stracture hai 
+ var a = [1,2,3,4]
+
+//  What is array
+// array is a data structure tht can store multiple values all at once place
+var arr = [1,2,3,4]
+
+// why array -> ek se jyada values store karne ke liye
+
+
+// who uses array -> devs
+
+// when to use array ->  whenever you need to store multiple values
+
+// how to create array -> []
+// how to access members of array
+
+var arr1 = [ 1, 2, 3, 4 ]
+// console.log(arr[1])
+// console.log(arr)
+var arr2  = new Array();
+arr2[0] = 1;
+arr2[1] = 2;
+console.log(arr2)
+
+// blank array banao
+ var  ar = [];
+//  ek array banao jisme 5 element ho
+var aarr = [1,2,3,4,5,6,7,8,9,10,11]
+
+var arr3 = new Array(20)
+console.log(arr3.length)
+
+
+for (var i=0; i<30; i++){
+    arr3[i] = i;
+}
+
+console.log(arr3)
