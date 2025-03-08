159ada1e6f0049b78494da762ff7fa3eb40f900d
himanshu6388 <himanshu219181@gmail.com>
2025-03-08 09:22:32 +0530

    updated

--- JsPractice/script.js
+++ JsPractice/script.js
@@ -1,8 +1,21 @@
-// Console and basic operations 
+ //1.  Console and basic operations 
 
-// print hello world three type
+ //1.1  print hello world three type
 
 console.log("Hello World");
 console.warn("Hello World");
 console.error("Hello World");
 console.info("Hello World");
+
+//  1.2 Perform 35*2-(10/2)+7 and log the result
+console.log(35*2 - (10/2) + 7)  // 72
+
+
+// 1.3 log the data tyep of "123", 123,  true, and null using typeof
+ console.log(typeof "123")
+ console.log(typeof 123)
+ console.log(typeof true)
+ console.log(typeof null)
+// 1.4 Write a program that swaps the values of two variasbles.
+
+// 1.5 Use console.group() to organize logs into groups.
