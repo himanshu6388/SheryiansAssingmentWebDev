faee0de9cf98822872c33bc8597e7732d1f54077
himanshu6388 <himanshu219181@gmail.com>
2025-03-08 09:06:37 +0530

    updated

---
+++ JsPractice/script.js
@@ -0,0 +1,8 @@
+// Console and basic operations 
+
+// print hello world three type
+
+console.log("Hello World");
+console.warn("Hello World");
+console.error("Hello World");
+console.info("Hello World");
